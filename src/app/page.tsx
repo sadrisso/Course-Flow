a52e32c361b86a125827d3a255bd8da8f241cfbc
sadrisso <sadriisso304@gmail.com>
2025-08-05 06:24:53 +0600

    added a static hero section with navbar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,10 @@
+import HeroSection from "@/components/heroSection/HeroSection";
 
 export default function Home() {
   return (
     <div>
       <div>
-        
+        <HeroSection />
       </div>
     </div>
   );
